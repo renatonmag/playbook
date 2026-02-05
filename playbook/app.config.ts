39ca162ba7cfe071088e21872d042423d1e83ca0
Renato Magalhães <renato.n.magalhaes@gmail.com>
2026-02-05 14:06:57 -0300

    Remove all notion copy files

--- playbook/app.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "@solidjs/start/config";
-import tailwindcss from "@tailwindcss/vite";
-import devtools from "solid-devtools/vite";
-
-export default defineConfig({
-  vite: {
-    plugins: [tailwindcss(), devtools()],
-  },
-});
